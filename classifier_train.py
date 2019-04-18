0171731397cec88bb0a1065e30c01fe1ece83d1a
Excaliburrrr <46273718+Excaliburrrr@users.noreply.github.com>
2019-04-18 15:04:49 +0800

    Update classifier_train.py

--- classifier_train.py
+++ classifier_train.py
@@ -1,24 +1,3 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-
-from data import Hand
-from utils import Trainer
-from network import resnet34, resnet101
-
-# Hyper-params
-data_root = './data/'
-model_path = './models/'
-batch_size = 60  # batch_size per GPU, if use GPU mode; resnet34: batch_size=120
-num_workers = 2
-
-init_lr = 0.01
-lr_decay = 0.8
-momentum = 0.9
-weight_decay = 0.000
-nesterov = True
-
 # Set Training parameters
 params = Trainer.TrainParams()
 params.max_epoch = 1000
@@ -26,6 +5,7 @@ params.criterion = nn.CrossEntropyLoss()
 params.gpus = [0]  # set 'params.gpus=[]' to use CPU mode
 params.save_dir = model_path
 params.ckpt = None
+cur_epoch = 0
 params.save_freq_epoch = 100
 
 # load data
@@ -42,20 +22,23 @@ val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_
 print('val dataset len: {}'.format(len(val_dataloader.dataset)))
 
 # models
-# model = resnet34(pretrained=False, modelpath=model_path, num_classes=1000)  # batch_size=120, 1GPU Memory < 7000M
-# model.fc = nn.Linear(512, 6)
-model = resnet101(pretrained=False, modelpath=model_path, num_classes=1000)  # batch_size=60, 1GPU Memory > 9000M
+#model = resnet34(pretrained=False, modelpath=model_path, num_classes=1000)
+#batch_size=120, 1GPU Memory < 7000M
+#model.fc = nn.Linear(512, 6)
+#model = resnet101(pretrained=False, modelpath=model_path, num_classes=1000)  # batch_size=60, 1GPU Memory > 9000M
+#model.fc = nn.Linear(512*4, 6)
+model = resnet50(pretrained=False, modelpath=model_path, num_classes=1000)
 model.fc = nn.Linear(512*4, 6)
-
 # optimizer
 trainable_vars = [param for param in model.parameters() if param.requires_grad]
-print("Training with sgd")
-params.optimizer = torch.optim.SGD(trainable_vars, lr=init_lr,
-                                   momentum=momentum,
-                                   weight_decay=weight_decay,
-                                   nesterov=nesterov)
+print("Training with Adam")
+# params.optimizer = torch.optim.SGD(trainable_vars, lr=init_lr,
+#                                    momentum=momentum,
+#                                    weight_decay=weight_decay,
+#                                    nesterov=nesterov)
+params.optimizer = torch.optim.Adam(trainable_vars, lr=init_lr, betas=(0.9, 0.99), weight_decay=weight_decay)
 
 # Train
 params.lr_scheduler = ReduceLROnPlateau(params.optimizer, 'min', factor=lr_decay, patience=10, cooldown=10, verbose=True)
-trainer = Trainer(model, params, train_dataloader, val_dataloader)
+trainer = Trainer(model, params, train_dataloader, cur_epoch, val_dataloader)
 trainer.train()
