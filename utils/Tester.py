b58d58279a4c4334abde18e52330d6d53e6235e5
Excaliburrrr <46273718+Excaliburrrr@users.noreply.github.com>
2019-04-18 15:07:00 +0800

    增加计算训练集和测试集的accuracy

--- utils/Tester.py
+++ utils/Tester.py
@@ -6,9 +6,10 @@ from .log import logger
 
 import torch
 from torch.autograd import Variable
+import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms.functional as tv_F
-
+import torch.backends.cudnn as cudnn
 
 class TestParams(object):
     # params based on your local env
@@ -23,24 +24,30 @@ class Tester(object):
 
     TestParams = TestParams
 
-    def __init__(self, model, test_params):
+    def __init__(self, model, test_params, test_data, train_data):
         assert isinstance(test_params, TestParams)
         self.params = test_params
-
+        self.test_data = test_data
+        self.train_data = train_data
         # load model
+
         self.model = model
         ckpt = self.params.ckpt
-        if ckpt is not None:
-            self._load_ckpt(ckpt)
-            logger.info('Load ckpt from {}'.format(ckpt))
 
         # set CUDA_VISIBLE_DEVICES, 1 GPU is enough
         if len(self.params.gpus) > 0:
             gpu_test = str(self.params.gpus[0])
             os.environ['CUDA_VISIBLE_DEVICES'] = gpu_test
             logger.info('Set CUDA_VISIBLE_DEVICES to {}...'.format(gpu_test))
+            self.model = nn.DataParallel(self.model)
             self.model = self.model.cuda()
-
+            if ckpt is not None:
+                self._load_ckpt(ckpt)
+                logger.info('Load ckpt from {}'.format(ckpt))
+        else:
+            if ckpt is not None:
+                self._load_ckpt(ckpt)
+                logger.info('Load ckpt from {}'.format(ckpt))
         self.model.eval()
 
     def test(self):
@@ -61,7 +68,46 @@ class Tester(object):
             score = F.softmax(output, dim=1)
             _, prediction = torch.max(score.data, dim=1)
 
-            print('Prediction number: ' + str(prediction[0]))
+            print('Prediction number: ' + str(prediction.item()))
+    
+    def accuracy_for_testdata(self):
+        accuracy_sum = 0
+        for step, (data, label) in enumerate(self.test_data):
+            # train model
+            inputs = Variable(data)
+            target = Variable(label)
+            if len(self.params.gpus) > 0:
+                inputs = inputs.cuda()
+                target = target.cuda()
+
+            # forward
+            score = F.softmax(self.model(inputs), dim=1)
+            _,score = torch.max(score.data, dim=1)
+            accuracy_sum += sum(score.cpu().data.numpy() == target.cpu().data.numpy())/target.size(0)
+            print(target.size(0))
+
+        print(step)
+        accuracy = accuracy_sum/(step+1)
+        print('The Test Accuracy=%.4f' % accuracy)
+
+    def accuracy_for_traindata(self):
+        accuracy_sum = 0
+        for step, (data, label) in enumerate(self.train_data):
+            # train model
+            inputs = Variable(data)
+            target = Variable(label)
+            if len(self.params.gpus) > 0:
+                inputs = inputs.cuda()
+                target = target.cuda()
+
+            # forward
+            score = F.softmax(self.model(inputs), dim=1)
+            _, score = torch.max(score.data, dim=1)
+            accuracy_sum += sum(score.cpu().data.numpy() == target.cpu().data.numpy()) / target.size(0)
+
+        accuracy = accuracy_sum / (step+1)
+        print('The Train Accuracy=%.4f' % accuracy)
+    
 
     def _load_ckpt(self, ckpt):
         self.model.load_state_dict(torch.load(ckpt))
